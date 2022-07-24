c033a1f5b544d52715958e671ef4cce24658858f
CisseTanimou <tanimoucisse@gmail.com>
2022-07-24 22:48:57 +0000

    add comment on arithmetic and budget

--- freeCodeCamp_projects/arithmetic_formatter.py
+++ freeCodeCamp_projects/arithmetic_formatter.py
@@ -1,6 +1,6 @@
 import re
 
-"""
+"""heeeeeeeyyyyyy
 Create a function that receives a list of strings that are arithmetic problems and returns the problems arranged vertically and side-by-side. 
 The function should optionally take a second argument. When the second argument is set to True, the answers should be displayed.
     
--- freeCodeCamp_projects/budget.py
+++ freeCodeCamp_projects/budget.py
@@ -1,4 +1,4 @@
-"""
+"""It's me
   Complete the Category class in budget.py. It should be able to instantiate objects based on different budget categories like food, clothing, and entertainment. 
   When objects are created, they are passed in the name of the category. The class should have an instance variable called ledger that is a list. 
   The class should also contain the following methods:
